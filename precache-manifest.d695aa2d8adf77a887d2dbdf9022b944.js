ca667ae795097bada568da17038d25caeb729af4
Joel Stauffer <jstauffer@apterainc.com>
2020-09-15 12:56:55 -0400

    Deployment 2: Fixed hosting root

---
+++ precache-manifest.d695aa2d8adf77a887d2dbdf9022b944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7a5899942aecd3bd9c7a43d68bca48",
+    "url": "/secret-lorem/index.html"
+  },
+  {
+    "revision": "f59377fe628ddb5aab10",
+    "url": "/secret-lorem/static/css/main.381e690c.chunk.css"
+  },
+  {
+    "revision": "bffea45b72e6db6eb4b6",
+    "url": "/secret-lorem/static/js/2.9edebdce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/secret-lorem/static/js/2.9edebdce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59377fe628ddb5aab10",
+    "url": "/secret-lorem/static/js/main.6eda25da.chunk.js"
+  },
+  {
+    "revision": "30b8a2fd753a59d76d48",
+    "url": "/secret-lorem/static/js/runtime-main.5ab5032d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abd74999e633da6b84ca01b039c7ef60.js"
+  "/secret-lorem/precache-manifest.d695aa2d8adf77a887d2dbdf9022b944.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/secret-lorem/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
